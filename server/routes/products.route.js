64b3d481e937e1f7ee425451192edd73a7ec138c
ANIRUDHADITHYA <anirudhadithya.b.s@gmail.com>
2024-05-26 11:36:04 +0530

    updated

--- server/routes/products.route.js
+++ server/routes/products.route.js
@@ -7,14 +7,6 @@ const router = express.Router();
 //products endpoints
 router.get("/", getAllProducts);
 
-{/*
-router.get("/process/:process_id", getProductByProcessID);
-
-router.get("/category/:category_id", getProductByCategoryID);
-
-router.get("/product/:product_id", getProductByProductID);
- */}
-
 router.post("/add-product", verifyAdminAccessToken, addProduct);
 
 
