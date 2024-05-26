b8e5a544ec7629c63021af8508a2f8ead0d367a7
ANIRUDHADITHYA <anirudhadithya.b.s@gmail.com>
2024-05-26 17:06:51 +0530

    updated admin for test

--- admin/src/Pages/Login/Login.js
+++ admin/src/Pages/Login/Login.js
@@ -13,7 +13,7 @@ export default function Login() {
         e.preventDefault();
         try {
             await login(email, password);
-            navigate('/dashboard');
+            navigate('/admin/dashboard');
         } catch (error) {
             alert('Login failed');
             console.log(error)
